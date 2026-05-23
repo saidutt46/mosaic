38e2abae478dceec0e391a87822d9597cfc992a2
Sai Dutt G.V <duttganduri46@gmail.com>
2026-05-22 22:35:54 -0700

    Initial Commit

---
+++ Mosaic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mosaic
+//
+//  Created by gvs on 5/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mosaic/MosaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MosaicApp.swift
+//  Mosaic
+//
+//  Created by gvs on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MosaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
